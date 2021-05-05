0e930ec2385fa56e67b4be9ad60b96509804ea45
Alberto Moretti MSI <moretti919@gmail.com>
2021-05-05 19:36:50 +0200

    [init]

---
+++ config.py
@@ -0,0 +1,17 @@
+class Config(object):
+    FLASK_ENV = 'development'
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = 'sqlite:///:memory:'
+
+
+class ProductionConfig(Config):
+    DATABASE_URI = 'mysql://user@localhost/foo'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
