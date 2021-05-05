3b7df51b3dfe6ec5592bac67c49e40b228eb69d1
hiimjako <moretti919@gmail.com>
2021-05-05 22:39:47 +0200

    [wip] creating base project

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
+from os import environ
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from config import configDict
 
 app = Flask(__name__)
-app.config.from_object('config.DevelopmentConfig')
+app.config.from_object(configDict.get(environ.get('FLASK_CONFIG', default='DevelopmentConfig')))
+
+db = SQLAlchemy()
+db.init_app(app)
+# db.create_all()
 
 
-@app.route('/')
+@ app.route('/')
 def hello_world():
+    print(app.config)
     return render_template('base.html')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
--- config.py
+++ config.py
@@ -1,17 +1,31 @@
+from os import environ, path
+
+basedir = path.abspath(path.dirname(__file__))
+
+
 class Config(object):
-    FLASK_ENV = 'development'
     DEBUG = False
     TESTING = False
     DATABASE_URI = 'sqlite:///:memory:'
 
 
 class ProductionConfig(Config):
+    FLASK_ENV = 'production'
     DATABASE_URI = 'mysql://user@localhost/foo'
 
 
 class DevelopmentConfig(Config):
+    FLASK_ENV = 'development'
+    PORT = 10000
     DEBUG = True
 
 
 class TestingConfig(Config):
     TESTING = True
+
+
+configDict = {
+    'DevelopmentConfig': DevelopmentConfig,
+    'ProductionConfig': ProductionConfig,
+    'TestingConfig': TestingConfig,
+}
